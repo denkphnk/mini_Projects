ae1ed8326926c285933eb5400b970011a319b48a
daniil kachuk <daniilkacuk@gmail.com>
2022-12-21 18:30:36 +0200

    Calculator

---
+++ main.py
@@ -0,0 +1,12 @@
+print('Hello World!')
+a = int(input())
+b = int(input())
+c = input()
+if c == '+':
+    print(a + b)
+if c == '-':
+    print(a - b)
+if c == '*':
+    print(a * b)
+if c == '/':
+    print(a / b)
